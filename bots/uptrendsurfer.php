5571b2d86114fae250d58381570a5925ff6318f3
dandb2 <35116363+dandb2@users.noreply.github.com>
2020-08-27 10:47:40 +0200

    Update uptrendsurfer.php

--- bots/uptrendsurfer.php
+++ bots/uptrendsurfer.php
@@ -22,7 +22,7 @@
 *
 */
 
-include_once(dirname(__FILE__).'/../coinbaseExchange.php');
+include_once(dirname(__FILE__).'/../coinbase-pro.php');
 $g = new coinbaseExchange(CB_KEY,CB_SECRET,CB_PASSPHRASE);
 
 // check arguments and stuff
